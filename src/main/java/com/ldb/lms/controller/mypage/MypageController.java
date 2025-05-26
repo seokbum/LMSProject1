9ba71b83c8416a4cbd0e54a9a04242ecdbb8cd73
donggonyoo <ddkk8525@gmail.com>
2025-05-26 16:34:39 +0900

    donggon 16L33

--- src/main/java/com/ldb/lms/controller/mypage/MypageController.java
+++ src/main/java/com/ldb/lms/controller/mypage/MypageController.java
@@ -1,10 +1,21 @@
 package com.ldb.lms.controller.mypage;
 
+import java.util.List;
+
+import org.springframework.http.HttpRequest;
+import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.ldb.lms.dto.mypage.LoginDto;
 import com.ldb.lms.service.mypage.MypageService;
+import org.springframework.web.bind.annotation.PostMapping;
+
+
 
 @Controller
 @RequestMapping("/mypage")
@@ -19,8 +30,19 @@ public class MypageController {
 	
 	@GetMapping("doLogin")
 	public String callDoLogin () {
-		System.out.println("안녕하세요 doLogin");
+		//mypageService.loginChk();
 		return "mypage/doLogin";
+	}
+	
+	@PostMapping("login")
+	public String login(@RequestParam String studentId ) {
+		System.out.println(studentId );
+		//System.out.println(dto.getProfessorId());
+		//System.out.println(dto.getStudentId());
+	
+		return null;
+		
+		
 	}
 	
 	
