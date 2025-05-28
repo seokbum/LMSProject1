cbe4ffea660701adca1ba1f6fa40e4c140003469
limjuhan <starlexy@naver.com>
2025-05-28 17:45:23 +0900

    교수지원>강의등록 작업중

---
+++ src/main/java/com/ldb/lms/controller/professor_support/ProfeesorApiController.java
@@ -0,0 +1,40 @@
+package com.ldb.lms.controller.professor_support;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.extern.slf4j.Slf4j;
+
+
+@Slf4j
+@RestController
+@RequestMapping("/api/professors")
+public class ProfeesorApiController {
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
